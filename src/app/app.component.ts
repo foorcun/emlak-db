ba495fbf5c91c5179422adb5708bf893ec82fe26
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-26 00:14:41 +0300

    footer down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarBootstrapComponent } from './common/navbar-bootstrap/navbar-bootstrap.component';
+import { FooterComponent } from "./common/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarBootstrapComponent],
+  imports: [RouterOutlet, NavbarBootstrapComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
