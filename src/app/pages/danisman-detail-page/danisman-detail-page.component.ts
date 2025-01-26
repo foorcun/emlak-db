dfec2ef5db09558222119498a5bb805f94b420eb
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-26 18:42:58 +0300

    info cards

--- src/app/pages/danisman-detail-page/danisman-detail-page.component.ts
+++ src/app/pages/danisman-detail-page/danisman-detail-page.component.ts
@@ -12,29 +12,33 @@ import { DanismanHeroBeltComponent } from './danisman-hero-belt/danisman-hero-be
 export class DanismanDetailPageComponent implements OnInit {
   selectedDanisman: any;
 
-  danismanlar = [
-    {
-      id: 1,
-      photo: 'images/person.png',
-      name: 'Ahmet Yılmaz',
-      emlakOfisi: 'XYZ Emlak',
-      role: 'Saha Direktörü',
-    },
-    {
-      id: 2,
-      photo: 'images/person.png',
-      name: 'Elif Demir',
-      emlakOfisi: 'ABC Realty',
-      role: 'Broker Owner',
-    },
-    {
-      id: 3,
-      photo: 'images/person.png',
-      name: 'Mehmet Çelik',
-      emlakOfisi: 'Mega Emlak',
-      role: 'Gayrimenkul Danışmanı',
-    },
-  ];
+danismanlar = [
+  {
+    id: 1,
+    photo: 'images/person.png',
+    name: 'Ahmet Yılmaz',
+    emlakOfisi: 'XYZ Emlak',
+    role: 'Saha Direktörü',
+    experience: 10, // Years of experience
+  },
+  {
+    id: 2,
+    photo: 'images/person.png',
+    name: 'Elif Demir',
+    emlakOfisi: 'ABC Realty',
+    role: 'Broker Owner',
+    experience: 15, // Years of experience
+  },
+  {
+    id: 3,
+    photo: 'images/person.png',
+    name: 'Mehmet Çelik',
+    emlakOfisi: 'Mega Emlak',
+    role: 'Gayrimenkul Danışmanı',
+    experience: 5, // Years of experience
+  },
+];
+
 
   constructor(private route: ActivatedRoute) { }
 
--- src/app/pages/danisman-detail-page/danisman-hero-belt/danisman-hero-belt.component.ts
+++ src/app/pages/danisman-detail-page/danisman-hero-belt/danisman-hero-belt.component.ts
@@ -9,4 +9,5 @@ import { Component, Input } from '@angular/core';
 export class DanismanHeroBeltComponent {
   @Input() danismanName: string = '';
   @Input() danismanSubtitle: string = '';
+  danismanExperience: number = 10; // Years of experience
 }
